ca55a510674370cf93d6e922d7b71b8179e38416
balti salah <balti0salah@gmail.com>
2025-04-10 00:43:39 +0200

    add CICD4

--- main.tf
+++ main.tf
@@ -1,64 +1,74 @@
+locals {
+  env = terraform.workspace
+}
+
+
 resource "aws_vpc" "main2" {
   cidr_block = var.cidr_block
   tags = {
-    Name = var.vpc_name
+    Name = "${var.vpc_name}-${local.env}"
   }
 }
 
 # 2. Create Public Subnets (2x)
 resource "aws_subnet" "public" {
-  for_each = { for subnet in var.public_subnets : subnet.name => subnet }
+  count =  length(var.public_subnets)
 
+  
   vpc_id                  = aws_vpc.main2.id
-  cidr_block              = each.value.cidr_block
-  availability_zone       = each.value.availability_zone
+  cidr_block              = var.public_subnets[count.index].cidr_block
+  availability_zone       = var.public_subnets[count.index].availability_zone
   map_public_ip_on_launch = true  # Hardcoded for public subnets
 
   tags = {
-    Name = each.value.name
+    Name = "${var.public_subnets[count.index].name}-${local.env}"
   }
 }
 
 
 # 3. Create Private Subnets (2x)
 resource "aws_subnet" "private" {
-  for_each = { for subnet in var.private_subnets : subnet.name => subnet }
+  count =  length(var.private_subnets)
 
+  
   vpc_id                  = aws_vpc.main2.id
-  cidr_block              = each.value.cidr_block
-  availability_zone       = each.value.availability_zone
-  map_public_ip_on_launch = false  # Explicitly private
+  cidr_block              = var.private_subnets[count.index].cidr_block
+  availability_zone       = var.private_subnets[count.index].availability_zone
+  map_public_ip_on_launch = false  # Hardcoded for public subnets
 
   tags = {
-    Name = each.value.name
+    Name = "${var.private_subnets[count.index].name}-${local.env}"
   }
 }
+
 
 
 # 4. Internet Gateway (for public subnets)
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main2.id
   tags = {
-    Name = var.igw_name
+    Name = "${var.igw_name}-${local.env}"
   }
 }
 
 # 5. Elastic IP (for NAT Gateway)
 resource "aws_eip" "nat_eip" {
+  count =  length(var.private_subnets)
   domain = "vpc"
   tags = {
-    Name = "nat-gateway-eip"
+    Name = "nat-gateway-eip-${count.index}-${local.env}"
   }
 }
 
 # 6. NAT Gateway (placed in 1st public subnet)
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = values(aws_subnet.public)[0].id # Attach to public subnet
+  count =  length(var.private_subnets)
+  allocation_id = aws_eip.nat_eip[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id # Attach to public subnet
   depends_on    = [aws_internet_gateway.igw]  # Explicit dependency
 
   tags = {
-    Name = "main-nat"
+    Name = "main-nat-${count.index}-${local.env}"
   }
 }
 
@@ -72,36 +82,38 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "public-rt"
+    Name = "public-rt-${local.env}"
   }
 }
 
 # 8. Associate Public Subnets with Public Route Table
 resource "aws_route_table_association" "public" {
-  for_each = aws_subnet.public
+  count =  length(var.public_subnets)
 
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = element(aws_route_table.public[*].id, count.index)
 }
 
 # 9. Private Route Table (routes to NAT Gateway)
 resource "aws_route_table" "private" {
+  count =  length(var.private_subnets)
   vpc_id = aws_vpc.main2.id
+ 
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 
   tags = {
-    Name = "private-rt"
+    Name = "private-rt-${count.index}-${local.env}"
   }
 }
 
 # 10. Associate Private Subnets with Private Route Table
 resource "aws_route_table_association" "private" {
-  for_each = aws_subnet.private
 
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.private.id
+  count =  length(var.private_subnets)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = element(aws_route_table.private[*].id, count.index)
 }
