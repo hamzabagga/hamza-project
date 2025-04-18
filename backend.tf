950883dcedbb2d3385cec02e2c77d7dc58753512
Bentisha <mohamedaminebenticha30@gmail.com>
2025-04-18 17:55:37 +0200

    first deploy

--- backend.tf
+++ backend.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "salah-terraform-state-2025"
-    key          = "salah/terraform.tfstate"
+    bucket       = "med-state-2025"
+    key          = "med/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
