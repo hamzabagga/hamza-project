0a165d38fb752f38e8641331fe33135fd08350dc
Bentisha <mohamedaminebenticha30@gmail.com>
2025-04-19 23:52:31 +0200

    change bucket

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "example-terraform-frontend"
+    bucket       = "example-state-frontend"
     key          = "med/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
